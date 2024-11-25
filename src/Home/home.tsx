a30968b37576639fb40b1bba146657ecb51580c6
prab_neup <prabeshp848@gmail.com>
2024-11-25 15:21:42 +0545

    Add New text

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -6,6 +6,7 @@ export const Home = ({ count }: Hello) => {
   return (
     <div>
       <p className="text-slate-100 mt-4 text-8xl">React {count}</p>
+      <p>Learning new Project in react</p>
     </div>
   );
 };
